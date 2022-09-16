1942e2f82d884ee85bf5f1314a2071416ba5e040
FelipeODEUS <92820937+FelipeODEUS@users.noreply.github.com>
2022-09-16 17:53:41 -0300

    Add files via upload

---
+++ organizador.py
@@ -0,0 +1,7 @@
+import os 
+import shutil
+
+pastaAtual = "C:\Users\fcmar\OneDrive\Documentos\codeFelipe\aula do dia 16"
+pastaNova = "C:\Users\fcmar\OneDrive\Documentos\codeFelipe\aula do dia 16\teste"
+listaArq = os.listdir(pastaAtual)
+print(listaArq)
